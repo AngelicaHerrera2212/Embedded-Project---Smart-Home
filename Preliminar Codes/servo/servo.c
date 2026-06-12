0337695d542a47429afd905b663f259f52225287
AngelicaHerrera2212 <132586865+AngelicaHerrera2212@users.noreply.github.com>
2026-06-13 00:13:59 +0200

    Refactor servo control parameters and initialization

--- Preliminar Codes/servo/servo.c
+++ Preliminar Codes/servo/servo.c
@@ -1,26 +1,23 @@
 #include "servo.h"
 /* SERVO DOOR CONTROL MODULE
  * GPIO: PA8 -> Servo signal pin
- * Timer:
- * TIM1_CH1
- * Servo connection:
- * Brown/Black -> GND
- * Red         -> 5V from MB-102
- * Orange/Yellow -> PA8
- */
+ * Timer: TIM1_CH1
+ * Servo connection: Brown/Black -> GND
+ * Red -> 5V from MB-102
+ * Orange/Yellow -> PA8 */
 
 // Adjust these values by trial and error
-#define SERVO_CLOSED_ANGLE   0
-#define SERVO_OPEN_ANGLE     90
+#define SERVO_CLOSED_ANGLE   180
+#define SERVO_OPEN_ANGLE     0
 
 /* Servo pulse range:
  * Usually:
- * 0°   -> 0.5 ms
+ * 0°   -> 1 ms
  * 90°  -> 1.5 ms
- * 180° -> 2.5 ms
- */
-#define SERVO_MIN_PULSE_US   500
-#define SERVO_MAX_PULSE_US   2500
+ * 180° -> 2 ms */
+
+#define SERVO_MIN_PULSE_US   1000
+#define SERVO_MAX_PULSE_US   2000
 
 void Servo_Init(void)
 {
@@ -32,19 +29,16 @@ void Servo_Init(void)
 
     /* Configure PA8 as Alternate Function
      * PA8 MODER bits [17:16]
-     * 10 = alternate function
-     */
-    GPIOA->MODER &= ~(0x03 << 16);
-    GPIOA->MODER |=  (0x02 << 16);
+     * 10 = alternate function */
+    GPIOA->MODER &= ~(0x03 << 16); // Clear bits [17:16] - Apply Mask and force to 0
+    GPIOA->MODER |=  (0x02 << 16); // Set PA8 as OUTPUT - OR
 
-    /* Select AF1 for PA8
-     * PA8 -> TIM1_CH1 */
-    GPIOA->AFR[1] &= ~(0x0F << 0);
-    GPIOA->AFR[1] |=  (0x01 << 0);
+    // Select AF1 for PA8. PA8 -> TIM1_CH1
+    GPIOA->AFR[1] &= ~(0x0F << 0); // Clear bits [3:0] - Apply Mask and force to 0
+    GPIOA->AFR[1] |=  (0x01 << 0); //
 
-    /* Push-pull, no pull-up/down */
-    GPIOA->OTYPER &= ~(0x1 << 8);
-    GPIOA->PUPDR  &= ~(0x03 << 16);
+    GPIOA->OTYPER &= ~(0x1 << 8); // Push-pull,
+    GPIOA->PUPDR  &= ~(0x03 << 16); // no pull-up/down
 
     /* TIM1 PWM configuration
      * Timer clock assumed around 84 MHz.
@@ -52,10 +46,9 @@ void Servo_Init(void)
      * 1 tick = 1 microsecond.
      *
      * Servo needs 50 Hz:
-     * Period = 20 ms = 20000 us.
-     */
-    TIM1->PSC = 83;
-    TIM1->ARR = 19999;
+     * Period = 20 ms = 20000 us. */
+    TIM1->PSC = 84 - 1;
+    TIM1->ARR = 20000 - 1;
 
     // PWM mode 1 on channel 1
     TIM1->CCMR1 &= ~(0x7 << 4);
